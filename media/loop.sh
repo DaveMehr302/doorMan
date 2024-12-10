4a880c16664dc0073ca85593b694735533c6b631
Dave M <davme@gmx.ch>
2024-12-10 17:01:59 +0100

    added shell file

---
+++ media/loop.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#maybe set volume to full
+
+waitTime=5
+
+while:
+do
+	mpg321 Whisper.mp3
+	sleep waitTime
+
+	mpg321 mortalApproach.mp3
+	sleep waitTime
+
+	mpg321 EatYouAlive.mp3
+	sleep waitTime
+
+	mpg321 IamDeath.mp3
+	sleep waitTime
+
+
+
+	#Rauchmaschine
+
+done
