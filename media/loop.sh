8eebcf788973121e862e0cd7f260afa26bdeedc4
DaveMehr <davme@gmx.ch>
2024-12-14 13:41:16 +0100

    added Relais

--- media/loop.sh
+++ media/loop.sh
@@ -3,9 +3,21 @@
 #maybe set volume to full
 
 waitTime=5
+smokeTime=5
+
+#configure relais pin (Relays 2 => ch20)
+echo 20 > /sys/class/gpio/export
+echo out > /sys/class/gpio/gpio20/direction
 
 while :
 do
+
+	echo 0 > /sys/class/gpio/gpio20/value
+	sleep $smokeTime
+	echo 1 > /sys/class/gpio/gpio20/value
+
+
+
 	mpg321 Whisper.mp3
 	sleep $waitTime
 
