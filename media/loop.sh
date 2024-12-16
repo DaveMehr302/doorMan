455136991155d87584866d28e79695d680b16777
DaveMehr <davme@gmx.ch>
2024-12-16 20:14:00 +0100

    do sound twice and wait longer

--- media/loop.sh
+++ media/loop.sh
@@ -2,7 +2,7 @@
 
 #maybe set volume to full
 
-waitTime=5
+waitTime=20
 smokeTime=5
 
 #configure relais pin (Relays 2 => ch20)
@@ -14,6 +14,18 @@ do
 	echo 0 > /sys/class/gpio/gpio20/value
 	sleep $smokeTime
 	echo 1 > /sys/class/gpio/gpio20/value
+
+	mpg321 /home/pi/doorMan/src/doorMan/media/Whisper.mp3
+	sleep $waitTime
+
+	mpg321 /home/pi/doorMan/src/doorMan/media/mortalApproch.mp3
+	sleep $waitTime
+
+	mpg321 /home/pi/doorMan/src/doorMan/media/EatYouAlive.mp3
+	sleep $waitTime
+
+	mpg321 /home/pi/doorMan/src/doorMan/media/IamDeath.mp3
+	sleep $waitTime
 
 	mpg321 /home/pi/doorMan/src/doorMan/media/Whisper.mp3
 	sleep $waitTime
